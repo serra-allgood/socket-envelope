1debd8307e981ccf7d8a5275424283e84548ebb6
Serra Allgood <serra.allgood@gmail>
2018-09-04 13:04:36 -0700

    Follow a damn guide

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,13 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index.js'
-  },
+    path: path.join(__dirname, './dist'),
+    filename: 'index.js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true
+},
   module: {
     rules: [
       {
@@ -16,5 +20,18 @@ module.exports = {
         },
       }
     ]
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+    }
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    }
   }
 };
